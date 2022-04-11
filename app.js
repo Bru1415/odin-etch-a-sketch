daece3f5229169bac2cdd4ed6c6597ad404bc2cd
Bruno-Martin Schuster <bruno_schuster@gmx.de>
2022-04-11 22:09:58 +0200

    Center the container and test the hover-effect Hover via css not permanent

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const gE_gridContainer = document.querySelector('#container');
 
 for(let i = 0; i < 256; i++){
 const gridElement = document.createElement('div');
-gridElement.innerText = i;
+// gridElement.innerText = i;
 
 gE_gridContainer.append(gridElement);
 

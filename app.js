43f4db9adf239ca01a3a7d690615b6b5d70bf799
Bruno-Martin Schuster <bruno_schuster@gmx.de>
2022-04-11 20:53:11 +0200

    Add index.html css and app.js Create a 16x16 grid through js add some styles

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const gE_gridContainer = document.querySelector('#container');
+
+for(let i = 0; i < 256; i++){
+const gridElement = document.createElement('div');
+gridElement.innerText = i;
+
+gE_gridContainer.append(gridElement);
+
+
+}

d166a906fd8cb4a67891747676382f80c79b8250
Bruno-Martin Schuster <bruno_schuster@gmx.de>
2022-04-13 20:32:21 +0200

    Add menu-bar and event-lsitener for making grid Deafult grid is added on window-load. Add button for new empty grid

--- app.js
+++ app.js
@@ -1,28 +1,50 @@
-
 const gE_gridContainer = document.querySelector('#container');
+const gE_newGridBtn = document.querySelector('#new-grid');
 
-for(let i = 0; i < 256; i++){
-const gridElement = document.createElement('div');
-// gridElement.innerText = i;
+let gV_numberOfColumns = 16;
 
-gE_gridContainer.append(gridElement);
 
+gE_gridContainer.style.cssText = gE_gridContainer.style.cssText +
+    `grid-template-columns: repeat(${gV_numberOfColumns}, 6rem);grid-template-rows: repeat(${gV_numberOfColumns}, 6rem);`;
+
+
+
+
+const gF_makeGrid = (event) => {
+
+    for (let i = 0; i < gV_numberOfColumns ** 2; i++) {
+        const gridElement = document.createElement('div');
+        // gridElement.innerText = i;
+
+        gE_gridContainer.append(gridElement);
+
+    }
 
 }
+
+window.addEventListener('load', gF_makeGrid);
+
+gE_newGridBtn.addEventListener('click', (event) => {
+
+    document.getElementById('draw-area');
+    gV_numberOfColumns = prompt('How many columns do you want');
+    gF_makeGrid(event);
+
+})
 
 const gF_paint = (event) => {
     event.target.classList.add('paint');
 
 }
 
-gE_gridContainer.addEventListener('mousedown', (event)=>{
+gE_gridContainer.addEventListener('mousedown', (event) => {
 
-    gE_gridContainer.addEventListener('mouseover', gF_paint); 
+    gE_gridContainer.addEventListener('mouseover', gF_paint);
 
 })
 
 
-gE_gridContainer.addEventListener('mouseup', (event)=>{
+gE_gridContainer.addEventListener('mouseup', (event) => {
 
     gE_gridContainer.removeEventListener('mouseover', gF_paint);
 
